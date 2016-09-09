05fafa4dbb670c08fae0523181e31407c5883b03
zhou.jian <zekezhoujian@163.com>
2016-09-09 18:17:05 +0800

    zeke  提交代码

--- PicassoDemo/src/com/example/picassodemo/MainActivity.java
+++ PicassoDemo/src/com/example/picassodemo/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.picassodemo;
 
 import com.squareup.picasso.Picasso;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +8,11 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
 	private ImageView imageview;
 	private Button bt_button;
-
 	//http://square.github.io/picasso/
 	
 	@Override
@@ -22,15 +21,11 @@ public class MainActivity extends Activity {
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
 		bt_button = (Button) findViewById(R.id.bt_button);
 		imageview = (ImageView) findViewById(R.id.imageview);
-
 		bt_button.setOnClickListener(new OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
-
 				Picasso.with(getApplicationContext()).load("http://e.hiphotos.bdimg.com/album/h%3D370%3Bq%3D90/sign=3de7849d6d81800a71e58f09810e42c7/279759ee3d6d55fb5e440b346c224f4a20a4dd26.jpg")
 				//Ĭ��ռλͼ
 				.placeholder(R.drawable.ic_launcher)
